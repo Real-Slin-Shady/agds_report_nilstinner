ec9252746500095fe04a6e1639c4b81a51bb7003
Nils <slin@stud-207-97.eduroam.unibe.ch>
2023-05-02 16:35:54 +0200

    Finished ml_1 first draft and ploish of regr report

--- R/Evaluate_model_function.R
+++ R/Evaluate_model_function.R
@@ -1,4 +1,4 @@
-eval_model <- function(mod, df_train, df_test){
+eval_model <- function(mod, df_train, df_test,out){
   
   # add predictions to the data frames
   df_train <- df_train |> 
@@ -37,7 +37,7 @@ eval_model <- function(mod, df_train, df_test){
   # adding information of metrics as sub-titles
   plot_1 <- ggplot(data = df_train, aes(GPP_NT_VUT_REF, fitted)) +
     geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
+    geom_smooth(method = "lm",formula = 'y ~ x', se = FALSE, color = "red") +
     geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
     labs(subtitle = bquote( italic(R)^2 == .(format(rsq_train, digits = 2)) ~~
                               RMSE == .(format(rmse_train, digits = 3))),
@@ -46,7 +46,7 @@ eval_model <- function(mod, df_train, df_test){
   
   plot_2 <- ggplot(data = df_test, aes(GPP_NT_VUT_REF, fitted)) +
     geom_point(alpha = 0.3) +
-    geom_smooth(method = "lm", se = FALSE, color = "red") +
+    geom_smooth(method = "lm",formula = 'y ~ x', se = FALSE, color = "red") +
     geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
     labs(subtitle = bquote( italic(R)^2 == .(format(rsq_test, digits = 2)) ~~
                               RMSE == .(format(rmse_test, digits = 3))),
@@ -56,15 +56,19 @@ eval_model <- function(mod, df_train, df_test){
   
   plot_3 <- ggplot(data = df_train, aes(TIMESTAMP, fitted-GPP_NT_VUT_REF)) +
     geom_point(alpha = 0.3) +
-    stat_smooth(method="loess", na.rm=TRUE)+
+    stat_smooth(method="loess",formula = 'y ~ x', na.rm=TRUE)+
     theme_classic()
   
   plot_4 <- ggplot(data = df_test, aes(TIMESTAMP, fitted-GPP_NT_VUT_REF)) +
     geom_point(alpha = 0.3) +
-    stat_smooth(method="loess", na.rm=TRUE)+
+    stat_smooth(method="loess",formula = 'y ~ x', na.rm=TRUE)+
     theme_classic()
   
-  out <- cowplot::plot_grid(plot_1, plot_2, plot_3,plot_4)
   
-  return(out)
+  if (out == "plot_all"){return(cowplot::plot_grid(plot_1, plot_2, plot_3,plot_4))}else if(out =="plot"){
+    return(cowplot::plot_grid(plot_1, plot_2))
+  }else if(out == "r.squared"){
+    return(list("rsq_train" = rsq_train,"rsq_test" = rsq_test))
+  }
+  return()
 }
--- R/model_daily_fluxes.R
+++ R/model_daily_fluxes.R
@@ -1,7 +1,7 @@
 
-KNN_daily_fluxes <- function(tidyDF,kofKNN,split_prop){
+model_daily_fluxes <- function(tidyDF,method_loc,kofKNN,split_prop){
 # Data splitting
-set.seed(123)  # for reproducibility
+set.seed(1982)  # for reproducibility
 split <- rsample::initial_split(daily_fluxes, prop = split_prop, strata = "VPD_F")
 daily_fluxes_train <- rsample::training(split)
 daily_fluxes_test <- rsample::testing(split)
@@ -9,21 +9,31 @@ daily_fluxes_test <- rsample::testing(split)
 # Model and pre-processing formulation, use all variables but LW_IN_F
 pp <- recipes::recipe(GPP_NT_VUT_REF ~ SW_IN_F + VPD_F + TA_F, 
                       data = daily_fluxes_train |> drop_na()) |> 
-  recipes::step_BoxCox(all_predictors()) |> 
+  recipes::step_BoxCox(c(all_predictors(),-TA_F)) |> 
   recipes::step_center(all_numeric(), -all_outcomes()) |>
   recipes::step_scale(all_numeric(), -all_outcomes())
 
 
+
+if(method_loc =="knn"){
 # Fit KNN model
-mod_knn <- caret::train(
+mod <- caret::train(
   pp, 
   data = daily_fluxes_train |> drop_na(), 
-  method = "knn",
+  method = method_loc,
   trControl = caret::trainControl(method = "none"),
   tuneGrid = data.frame(k = kofKNN),
-  metric = "RMSE"
-)
+  metric = "RMSE")
+} else {
+  mod <- caret::train(
+    pp, 
+    data = daily_fluxes_train |> drop_na(), 
+    method = method_loc,
+    trControl = caret::trainControl(method = "none"),
+    metric = "RMSE"
+  )
+}
 
 
-return(list("mod_knn" = mod_knn,"daily_fluxes_train" = daily_fluxes_train,"daily_fluxes_test" = daily_fluxes_test))
+return(list("mod" = mod,"daily_fluxes_train" = daily_fluxes_train,"daily_fluxes_test" = daily_fluxes_test))
 }
