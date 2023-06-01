7a67028f94817153597c447ae939edb284b365bb
Nils <slin@stud-207-97.eduroam.unibe.ch>
2023-06-01 13:40:47 +0200

    Maybe solved error in install packages...

--- R/load_packages.R
+++ R/load_packages.R
@@ -6,7 +6,7 @@ load_packages <-  function(packages){
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
+  install.packages(packages[!installed_packages], repos = "http://cran.us.r-project.org")
 }
 invisible(lapply(packages, library, character.only = TRUE)) #load packages
 print("All packages installed and loaded")
